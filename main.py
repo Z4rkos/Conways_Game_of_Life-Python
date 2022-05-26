285609a42a42c4a8ba0b5e60ae25f3403350a90a
Z4rkos <75321188+Z4rkos@users.noreply.github.com>
2022-05-26 12:42:21 +0200

    Some formatting

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ SCALE = 12
 CELL_WIDTH = SCALE
 CELL_OFFSET = 1
 
-fps = 7
+fps = 7                         # Change this to change the speed of the game.
 clock = pygame.time.Clock()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GRAY = (140, 140, 140)
-RED = (255, 0, 0)
 
 rows = W_WIDTH // SCALE
 
@@ -80,42 +78,45 @@ def main():
                 run = False
                 sys.exit()
 
-            elif event.type == pygame.MOUSEBUTTONDOWN:               # Mouse button to add or remove a cell.
+            # Controls
+            # Mouse button to add or remove a cell.
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 column = pos[0] // (CELL_WIDTH + CELL_OFFSET)
                 row = pos[1] // (CELL_WIDTH + CELL_OFFSET)
 
-                if grid[row][column] == 0:
-                    grid[row][column] = 1
-                else:
-                    grid[row][column] = 0
+                grid[row][column] = 1 if grid[row][column] == 0 else 0
 
+            # End to reset the matrix.
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_END:                       # End to reset the matrix.
+                if event.key == pygame.K_END:                       
                     grid = [[0 for x in range(rows)] for y in range(collumns)]
                     game = False
 
                 elif event.key == pygame.K_r:
-                    grid = [[(random.randint(0, 1)) for x in range(rows)] for y in range(collumns)]
-
-                # if event.key == pygame.K_s and not saved:
-                #     saved = True
-                #     saved_grid = grid[:]
-                # elif event.key == pygame.K_s and saved:
-                #     saved = False
-                #     grid = saved_grid
-                #     print(grid)
-
-                if event.key == pygame.K_KP_ENTER and not game:     # Enter (the one by the num pad) to start and pause.
+                    grid = [[(random.randint(0, 1)) for x in range(rows)]
+                            for y in range(collumns)]
+                 """
+                 if event.key == pygame.K_s and not saved:
+                     saved = True
+                     saved_grid = grid[:]
+                 elif event.key == pygame.K_s and saved:
+                     saved = False
+                     grid = saved_grid
+                     print(grid)
+                """
+                # Enter to start and pause.
+                if event.key == pygame.K_RETURN and not game:     
                     game = True
-                elif event.key == pygame.K_KP_ENTER and game:
+                    pygame.display.set_caption("The Game Of Life (Running)")
+                elif event.key == pygame.K_RETURN and game:
                     game = False
+                    pygame.display.set_caption("The Game Of Life (Paused)")
 
         for row in range(rows):
             for column in range(collumns):
-                color = WHITE
-                if grid[row][column] == 1:
-                    color = BLACK
+                color = BLACK if grid[row][column] == 1 else WHITE
+
                 pygame.draw.rect(screen,
                                  color,
                                  [(CELL_OFFSET + CELL_WIDTH) * column + CELL_OFFSET,
